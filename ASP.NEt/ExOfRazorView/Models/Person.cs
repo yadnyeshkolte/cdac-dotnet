9e84bfa58759246762069373cc69f9e7c443be47
Yadnyesh <yadnyeshkolte@gmail.com>
2025-12-22 09:52:20 +0530

    updated

---
+++ ASP.NEt/ExOfRazorView/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace ExOfRazorView.Models
+{
+    public class Person
+    {
+
+        public string? name { get; set; }
+
+        public DateTime dob { get; set; }
+
+        public Gender personGender { get; set; }
+    }
+    public enum Gender { 
+        Male, Female, Others
+    }
+}
